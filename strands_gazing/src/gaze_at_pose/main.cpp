cdea91ba0d2482319c5e668961b103c25f480a37
cdondrup <cdondrup@lincoln.ac.uk>
2013-08-31 17:59:23 +0100

    Combining gazing and velocity in one branch. Adding dependencies to velocity package.

--- strands_gazing/src/gaze_at_pose/main.cpp
+++ strands_gazing/src/gaze_at_pose/main.cpp
@@ -5,17 +5,68 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <sensor_msgs/JointState.h>
 #include <tf/transform_listener.h>
+#include <actionlib/server/simple_action_server.h>
+#include <strands_gazing/GazeAtPoseAction.h>
+
+#include <limits>
 
 using namespace std;
 
 ros::Publisher head_pose_pub;
 tf::TransformListener* listener;
-
+actionlib::SimpleActionServer<strands_gazing::GazeAtPoseAction> *as_;
+boost::shared_ptr<const strands_gazing::GazeAtPoseGoal> goal_;
+strands_gazing::GazeAtPoseFeedback feedback_;
+strands_gazing::GazeAtPoseResult result_;
+std::string action_name_;
+bool once = false;
 string target_frame;
+double end_time;
+
+void goalCallback() {
+    goal_ = as_->acceptNewGoal();
+    ROS_INFO_STREAM("Received goal:\n" << *goal_);
+    end_time = goal_->runtime_sec > 0 ? ros::Time::now().toSec() + goal_->runtime_sec : 0.0;
+}
+
+void preemptCallback() {
+    ROS_INFO("%s: Preempted", action_name_.c_str());
+    // set the action state to preempted
+    sensor_msgs::JointState state;
+    state.header.frame_id = "/head_base_frame";
+    state.header.stamp = ros::Time::now();
+    state.name.push_back("HeadPan");
+    state.name.push_back("HeadTilt");
+    state.name.push_back("EyesPan");
+    state.position.push_back(0.0);
+    state.position.push_back(0.0);
+    state.position.push_back(0.0);
+
+    head_pose_pub.publish(state);
+    result_.expired = false;
+    as_->setPreempted(result_);
+}
+
+void inline feedback(geometry_msgs::Pose pose) {
+    feedback_.target = pose;
+    feedback_.remaining_time = end_time > 0 ? end_time - ros::Time::now().toSec() : INFINITY;
+    as_->publishFeedback(feedback_);
+}
+
+void inline checkTime() {
+    if(ros::Time::now().toSec() > end_time && end_time > 0.0) {
+        ROS_INFO("Execution time has been reached. Goal terminated successfully");
+        result_.expired = true;
+        as_->setSucceeded(result_);
+    }
+}
 
 
 void callback(const geometry_msgs::PoseArray::ConstPtr &msg)
 {
+    if (!as_->isActive())
+        return;
+
     ROS_INFO_STREAM("Received posearray:\n" << *msg);
 
     bool found = false;
@@ -28,6 +79,7 @@ void callback(const geometry_msgs::PoseArray::ConstPtr &msg)
         if(closest.pose.position.z > msg->poses[i].position.z) {
             closest.pose.position = msg->poses[i].position;
             found = true;
+            once = false;
         }
     }
 
@@ -47,19 +99,29 @@ void callback(const geometry_msgs::PoseArray::ConstPtr &msg)
         }
 
         ROS_INFO_STREAM("Transformed into:\n" << closest_head_coord);
-    } else {
+        feedback(closest_head_coord.pose);
+        checkTime();
+    } else if(!once){
         closest_head_coord = closest;
         closest_head_coord.header.frame_id = target_frame;
         closest_head_coord.pose.position.z = 0;
+        feedback(closest_head_coord.pose);
+        once = true;
+        checkTime();
+    } else {
+        feedback(closest_head_coord.pose);
+        checkTime();
+        return;
     }
+
 
     sensor_msgs::JointState state;
     state.header = closest_head_coord.header;
     state.name.push_back("HeadPan");
     state.name.push_back("HeadTilt");
-    state.position.push_back(std::atan2(closest_head_coord.pose.position.y, closest_head_coord.pose.position.x) * 180.0 / M_PI);
+    state.position.push_back(std::atan2(closest_head_coord.pose.position.y, closest_head_coord.pose.position.x) * 180.0 / M_PI * 0.8);
     state.position.push_back(std::atan2(closest_head_coord.pose.position.z, closest_head_coord.pose.position.x) * 180.0 / M_PI);
-
+    state.position.push_back(std::atan2(closest_head_coord.pose.position.y, closest_head_coord.pose.position.x) * 180.0 / M_PI * 0.2);
 
     head_pose_pub.publish(state);
 }
@@ -69,10 +131,15 @@ void callback(const geometry_msgs::PoseArray::ConstPtr &msg)
 int main(int argc, char **argv)
 {
     // Set up ROS.
-    ros::init(argc, argv, "gaze_at_pose");
+    action_name_ = "gaze_at_pose";
+    ros::init(argc, argv, action_name_.c_str());
     ros::NodeHandle n;
 
     listener = new tf::TransformListener();
+
+    as_ = new actionlib::SimpleActionServer<strands_gazing::GazeAtPoseAction>(n, action_name_, false);
+    as_->registerGoalCallback(boost::bind(&goalCallback));
+    as_->registerPreemptCallback(boost::bind(&preemptCallback));
 
     // Declare variables that can be modified by launch file or command line.
     string pose_array_topic;
@@ -83,13 +150,15 @@ int main(int argc, char **argv)
     // while using different parameters.
     ros::NodeHandle private_node_handle_("~");
     private_node_handle_.param("pose_array", pose_array_topic, string("/upper_body_detector/bounding_box_centres"));
-    private_node_handle_.param("head_pose", head_pose_topic, string("/gaze_at_pose/head_pose"));
+    private_node_handle_.param("head_pose", head_pose_topic, string("/head/commanded_state"));
     private_node_handle_.param("head_frame", target_frame, string("/head_base_frame"));
 
     // Create a subscriber.
     ros::Subscriber pose_array_sub = n.subscribe(pose_array_topic.c_str(), 10, &callback);
 
     head_pose_pub = n.advertise<sensor_msgs::JointState>(head_pose_topic.c_str(), 10);
+
+    as_->start();
 
     ros::spin();
     return 0;
