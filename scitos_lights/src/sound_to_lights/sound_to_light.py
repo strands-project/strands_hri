583a2554ac01276eba52b1a2f436ac94ee6c854b
Christian Dondrup <cdondrup@lincoln.ac.uk>
2013-10-30 09:46:44 +0000

    Added spinning light and enabled the action server functionality.

--- scitos_lights/src/sound_to_lights/sound_to_light.py
+++ scitos_lights/src/sound_to_lights/sound_to_light.py
@@ -40,13 +40,45 @@ class SoundLights():
         rospy.loginfo("Received goal:\n%s", self._goal)
         current_time = rospy.get_time()
         self.end_time = current_time + self._goal.seconds if self._goal.seconds > 0 else -1.0
-        #turn off lights
+        self.switchLEDs(0)
 
     def preemptCallback(self):
         rospy.loginfo("Cancelled execution of goal:\n%s", self._goal)
-        #reset lights to rotate
+        self.spinningLEDs()
         self._result.expired = False
         self._as.set_preempted(self._result)
+
+    def spinningLEDs(self):
+        lights = HeadLightState()
+        lights.HeadLightInterval = 80
+        lights.LEDState0 = 3
+        lights.LEDState1 = 3
+        lights.LEDState2 = 3
+        lights.LEDState3 = 3
+        lights.LEDState4 = 3
+        lights.LEDState5 = 3
+        lights.LEDState6 = 3
+        lights.LEDState7 = 3
+        lights.LEDPhase0 = 10
+        lights.LEDPhase1 = 20
+        lights.LEDPhase2 = 30
+        lights.LEDPhase3 = 40
+        lights.LEDPhase4 = 50
+        lights.LEDPhase5 = 60
+        lights.LEDPhase6 = 70
+        lights.LEDPhase7 = 80
+        lights.LEDAmplitude0 = 1.0
+        lights.LEDAmplitude1 = 1.0
+        lights.LEDAmplitude2 = 1.0
+        lights.LEDAmplitude3 = 1.0
+        lights.LEDAmplitude4 = 1.0
+        lights.LEDAmplitude5 = 1.0
+        lights.LEDAmplitude6 = 1.0
+        lights.LEDAmplitude7 = 1.0
+        if not rospy.is_shutdown():
+            self.pub.publish(lights)
+
+
 
     def switchLEDs(self, numLEDs):
         lights = HeadLightState()
@@ -84,12 +116,19 @@ class SoundLights():
     def grabAudioLevel(self):
         monitor = PeakMonitor(self.sink_name, self.meter_rate)
         for sample in monitor:
-            lights = float(sample) / float(self.max_sample_value) * 20
-            sample = sample >> self.display_scale
-            bar = '>' * sample
-            spaces = ' ' * (self.max_spaces - sample)
-            print ' %3d %s%s\r' % (sample, bar, spaces),
+            if not self._as.is_active():
+                rospy.logdebug("No active goal")
+                continue
+            lights = float(sample) / float(self.max_sample_value) * 20 #Magic number. Lets the light look good while speaking.
             self.switchLEDs(lights)
+            self._feedback.output_level = sample
+            self._feedback.remaining_runtime = self.end_time - rospy.get_time() if self.end_time > 0 else float('Inf')
+            self._as.publish_feedback(self._feedback)
+            if rospy.get_time() > self.end_time and self.end_time > 0:
+                rospy.loginfo("Execution time has been reached. Goal terminated successfully")
+                self.spinningLEDs()
+                self._result.expired = True
+                self._as.set_succeeded(self._result)
 
 
 if __name__ == '__main__':
