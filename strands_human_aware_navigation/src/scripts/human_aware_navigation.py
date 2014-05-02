d4f47aac628ceb699607f37127093713394098c5
Christian Dondrup <cdondrup@lincoln.ac.uk>
2014-05-02 16:31:40 +0100

    Adapted human_aware_navigation to be used as navigation action server in topological map.

--- strands_human_aware_navigation/src/scripts/human_aware_navigation.py
+++ strands_human_aware_navigation/src/scripts/human_aware_navigation.py
@@ -4,20 +4,18 @@ import rospy
 import actionlib
 import dynamic_reconfigure.client
 from strands_perception_people_msgs.msg import PedestrianLocations
-import strands_human_aware_velocity.msg
+import move_base_msgs.msg
+import thread
+import actionlib_msgs.msg
 
 class DynamicVelocityReconfigure():
     "A calss to reconfigure the velocity of the DWAPlannerROS."
-
-    # Create feedback and result messages
-    _feedback = strands_human_aware_velocity.msg.HumanAwareVelocityFeedback()
-    _result   = strands_human_aware_velocity.msg.HumanAwareVelocityResult()
 
     def __init__(self, name):
         rospy.loginfo("Starting %s", name)
         self._action_name = name
         rospy.loginfo("Creating action server.")
-        self._as = actionlib.SimpleActionServer(self._action_name, strands_human_aware_velocity.msg.HumanAwareVelocityAction, execute_cb = None, auto_start = False)
+        self._as = actionlib.SimpleActionServer(self._action_name, move_base_msgs.msg.MoveBaseAction, execute_cb = None, auto_start = False)
         self._as.register_goal_callback(self.goalCallback)
         self._as.register_preempt_callback(self.preemptCallback)
         rospy.loginfo(" ...starting")
@@ -29,6 +27,38 @@ class DynamicVelocityReconfigure():
         rospy.loginfo("Creating dynamic reconfigure client")
         self.client = dynamic_reconfigure.client.Client("/move_base/DWAPlannerROS")
         rospy.loginfo(" ...done")
+        rospy.loginfo("Creating base movement client.")
+        self.baseClient = actionlib.SimpleActionClient(
+            'move_base',
+            move_base_msgs.msg.MoveBaseAction
+        )
+        self.baseClient.wait_for_server()
+        rospy.loginfo("...done")
+        rospy.loginfo("Reading move_base parameters")
+        self.getCurrentSettings()
+        rospy.loginfo("Reading parameters")
+        self.threshold = rospy.get_param(name+"/timeout",5.0)
+        current_time = rospy.get_time()
+        self.timeout = current_time + self.threshold
+        self.max_dist = rospy.get_param(name+"/max_dist",5.0)
+        self.min_dist = rospy.get_param(name+"/min_dist",1.2)
+
+    def getCurrentSettings(self):
+        max_vel_x = round(rospy.get_param("/move_base/DWAPlannerROS/max_vel_x"), 2)
+        max_trans_vel = round(rospy.get_param("/move_base/DWAPlannerROS/max_trans_vel"),2)
+        max_rot_vel = round(rospy.get_param("/move_base/DWAPlannerROS/max_rot_vel"), 2)
+        min_vel_x = round(rospy.get_param("/move_base/DWAPlannerROS/min_vel_x"), 2)
+        min_trans_vel = round(rospy.get_param("/move_base/DWAPlannerROS/min_trans_vel"),2)
+        min_rot_vel = round(rospy.get_param("/move_base/DWAPlannerROS/min_rot_vel"), 2)
+        self.fast_param = {
+            'max_vel_x' : max_vel_x,
+            'max_trans_vel' : max_trans_vel,
+            'max_rot_vel' : max_rot_vel,
+            'min_vel_x' : min_vel_x,
+            'min_trans_vel' : min_trans_vel,
+            'min_rot_vel' : min_rot_vel
+        }
+        rospy.loginfo("Found following default values for move_base: %s", self.fast_param)
 
     def resetSpeed(self):
         rospy.logdebug("Resetting speeds to max:")
@@ -40,13 +70,9 @@ class DynamicVelocityReconfigure():
             rospy.logdebug("No active goal")
             return
 
-        self._feedback.num_found_humans = len(pl.poses)
-        self._feedback.min_dist = 0.0
-
         if len(pl.poses) > 0:
             rospy.logdebug("Found pedestrian: ")
             rospy.logdebug(" Pedestrian distance: %s", pl.min_distance)
-            self._feedback.min_dist = pl.min_distance
             factor = pl.min_distance - self.min_dist
             factor = factor if factor > 0.0 else 0.0
             factor /= (self.max_dist - self.min_dist)
@@ -54,11 +80,9 @@ class DynamicVelocityReconfigure():
             trans_speed = factor * self.max_vel_x
             trans_speed = round(trans_speed, 2)
             rospy.logdebug("Calculated translational speed: %s", trans_speed)
-            self._feedback.current_speed = trans_speed
             rot_speed = factor * self.max_rot_vel
             rot_speed = round(rot_speed, 2)
             rospy.logdebug("Calculated rotaional speed: %s", rot_speed)
-            self._feedback.current_rot = rot_speed
             if not trans_speed == self.max_vel_x and not rot_speed == self.max_rot_vel:
                 self.slow_param = {'max_vel_x' : trans_speed, 'max_trans_vel' : trans_speed, 'max_rot_vel' : rot_speed}
                 self.client.update_configuration(self.slow_param)
@@ -67,47 +91,46 @@ class DynamicVelocityReconfigure():
             self.timeout = rospy.get_time() + self.threshold
         elif rospy.get_time() > self.timeout:
             rospy.logdebug("Not found any pedestrians:")
-            self._feedback.current_speed = self.max_vel_x
             if not self.fast:
                 self.resetSpeed()
                 self.fast = True
             else:
                 rospy.logdebug(" Already fast")
 
-        self._feedback.remaining_runtime = self.end_time - rospy.get_time() if self.end_time > 0 else float('Inf')
-        self._feedback.time_to_reset = self.timeout - rospy.get_time()
-        self._feedback.time_to_reset = self._feedback.time_to_reset if self._feedback.time_to_reset > 0.0 else 0.0
-        self._as.publish_feedback(self._feedback)
-
-        if rospy.get_time() > self.end_time and self.end_time > 0:
-            rospy.loginfo("Execution time has been reached. Goal terminated successfully")
-            self.resetSpeed()
-            self._result.expired = True
-            self._as.set_succeeded(self._result)
-
     def goalCallback(self):
         self._goal = self._as.accept_new_goal()
+        self.getCurrentSettings()
         rospy.loginfo("Received goal:\n%s", self._goal)
-        self.threshold = self._goal.time_to_reset
-        current_time = rospy.get_time()
-        self.timeout = current_time + self.threshold
-        self.end_time = current_time + self._goal.seconds if self._goal.seconds > 0 else -1.0
-        self.max_vel_x = round(self._goal.max_vel_x, 2)
-        self.max_rot_vel = round(self._goal.max_rot_vel, 2)
-        self.max_dist = self._goal.max_dist
-        self.min_dist = self._goal.min_dist
-        self.fast_param = {'max_vel_x' : self.max_vel_x, 'max_trans_vel' : self.max_vel_x, 'max_rot_vel' : self.max_rot_vel}
-        self._feedback.current_speed = self.max_vel_x
         self.resetSpeed()
+        thread.start_new_thread(self.moveBaseThread,(self._goal,))
 
     def preemptCallback(self):
         rospy.loginfo("Cancelled execution of goal:\n%s", self._goal)
         self.resetSpeed()
-        self._result.expired = False
-        self._as.set_preempted(self._result)
+        self.baseClient.cancel_all_goals()
+        self._as.set_preempted()
+
+    def moveBaseThread(self,goal):
+        ret = self.moveBase(goal)
+        self.resetSpeed()
+        if not self._as.is_preempt_requested() and ret:
+            self._as.set_succeeded()
+        elif not ret:
+            self._as.set_aborted()
+
+    def moveBase(self,goal):
+        rospy.loginfo('Moving robot to goal: %s', goal)
+        self.baseClient.send_goal(goal)
+        self.baseClient.wait_for_result()
+        rospy.loginfo("Moved")
+        if self.baseClient.get_state() != actionlib_msgs.msg.GoalStatus.SUCCEEDED:
+            return False
+
+        rospy.sleep(rospy.Duration.from_sec(0.3)) #avoid jumping out of a state immediately after entering it - actionlib bug
+        return True
 
 
 if __name__ == '__main__':
-    rospy.init_node("human_aware_planner_velocities")
+    rospy.init_node("human_aware_navigation")
     dvr = DynamicVelocityReconfigure(rospy.get_name())
     rospy.spin()
