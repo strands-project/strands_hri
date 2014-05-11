9bbc8aaa2c50cbb36ebc90ba5b2fe686cf52c8e0
cdondrup <cdondrup@lincoln.ac.uk>
2014-05-11 16:42:08 +0100

    Typo

--- strands_interaction_behaviours/idle/src/idle_behaviour/idle_behaviour.py
+++ strands_interaction_behaviours/idle/src/idle_behaviour/idle_behaviour.py
@@ -88,7 +88,7 @@ class IdleBehaviour(object):
         strands_webserver.client_utils.display_relative_page(self.display_no, page)
         strands_webserver.client_utils.display_content(self.display_no, content)
 
-    def enageg(self,req):
+    def enage(self,req):
         a = std_msgs.msg.Bool()
         a.data = True
         self.pub.publish(a)
