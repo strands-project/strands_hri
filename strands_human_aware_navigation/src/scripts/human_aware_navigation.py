1824c000741c9b10811264e5adb634682dfe9f59
Bruno Lacerda <bruno@bruno-dell.(none)>
2014-05-13 14:33:32 +0100

    changes to human aware navigation to handle new goals wothout outputting failure

--- strands_human_aware_navigation/src/scripts/human_aware_navigation.py
+++ strands_human_aware_navigation/src/scripts/human_aware_navigation.py
@@ -41,14 +41,23 @@ class DynamicVelocityReconfigure():
         self.max_dist = rospy.get_param(name+"/max_dist",5.0)
         self.min_dist = rospy.get_param(name+"/min_dist",1.2)
         rospy.loginfo("Creating action server.")
-        self._as = actionlib.SimpleActionServer(self._action_name, move_base_msgs.msg.MoveBaseAction, execute_cb = None, auto_start = False)
-        self._as.register_goal_callback(self.goalCallback)
-        self._as.register_preempt_callback(self.preemptCallback)
+        self._as = actionlib.SimpleActionServer(self._action_name, move_base_msgs.msg.MoveBaseAction, self.goalCallback, auto_start = False)
+        #self._as.register_goal_callback()
+        #self._as.register_preempt_callback(self.preemptCallback)
         rospy.loginfo(" ...starting")
         self._as.start()
         rospy.loginfo(" ...done")
         sub_topic = rospy.get_param("~pedestrian_locations", '/pedestrian_localisation/localisations')
         rospy.Subscriber(sub_topic, PedestrianLocations, self.pedestrianCallback, None, 5)
+        
+        self.last_cancel_time=rospy.Time(0)
+        rospy.Subscriber("/human_aware_navigation/cancel" , actionlib_msgs.msg.GoalID, self.cancel_time_checker_cb)
+        
+        
+        
+    def cancel_time_checker_cb(self,msg):
+        self.last_cancel_time=rospy.get_rostime()
+        
 
     def getCurrentSettings(self):
         max_vel_x = round(rospy.get_param("/move_base/DWAPlannerROS/max_vel_x"), 2)
@@ -107,6 +116,7 @@ class DynamicVelocityReconfigure():
             if not trans_speed == self.fast_param['max_vel_x'] and not rot_speed == self.fast_param['max_rot_vel']:
                 self.slow_param = {'max_vel_x' : trans_speed, 'max_trans_vel' : trans_speed, 'max_rot_vel' : rot_speed}
                 try:
+                    print 'making it slow'
                     self.client.update_configuration(self.slow_param)
                 except rospy.ServiceException as exc:
                     rospy.logerr("Caught service exception: %s", exc)
@@ -121,8 +131,9 @@ class DynamicVelocityReconfigure():
             else:
                 rospy.logdebug(" Already fast")
 
-    def goalCallback(self):
-        self._goal = self._as.accept_new_goal()
+    def goalCallback(self,goal):
+        #self._goal = self._as.accept_new_goal()
+        self._goal=goal
         self.getCurrentSettings()
         rospy.logdebug("Received goal:\n%s", self._goal)
         self.resetSpeed()
@@ -130,14 +141,17 @@ class DynamicVelocityReconfigure():
         gaze_goal.runtime_sec = 0
         gaze_goal.topic_name = "/pose_extractor/pose"
         self.gazeClient.send_goal(gaze_goal)
-        thread.start_new_thread(self.moveBaseThread,(self._goal,))
+        #thread.start_new_thread(self.moveBaseThread,(self._goal,))
+        self.moveBaseThread(self._goal)
 
     def preemptCallback(self):
-        rospy.logdebug("Cancelled execution of goal:\n%s", self._goal)
-        self.baseClient.cancel_all_goals()
-        self.gazeClient.cancel_all_goals()
+        if rospy.get_rostime()-self.last_cancel_time < rospy.Duration(1):
+            rospy.logdebug("Cancelled execution of goal:\n%s", self._goal)
+            self.baseClient.cancel_all_goals()
+            self.gazeClient.cancel_all_goals()
+            self.resetSpeed()
         self._as.set_preempted()
-        self.resetSpeed()
+        
 
     def moveBaseThread(self,goal):
         ret = self.moveBase(goal)
@@ -151,9 +165,16 @@ class DynamicVelocityReconfigure():
     def moveBase(self,goal):
         rospy.logdebug('Moving robot to goal: %s', goal)
         self.baseClient.send_goal(goal, feedback_cb=self.moveBaseFeedbackCallback)
-        self.baseClient.wait_for_result()
-        self.result = self.baseClient.get_result()
-        if self.baseClient.get_state() != actionlib_msgs.msg.GoalStatus.SUCCEEDED:
+        status= self.baseClient.get_state()
+        while status==actionlib_msgs.msg.GoalStatus.PENDING or status==actionlib_msgs.msg.GoalStatus.ACTIVE:   
+            status= self.baseClient.get_state()
+            self.baseClient.wait_for_result(rospy.Duration(0.2))
+            self.result = self.baseClient.get_result()
+            if self._as.is_preempt_requested():
+                self.preemptCallback()
+                break
+                
+        if self.baseClient.get_state() != actionlib_msgs.msg.GoalStatus.SUCCEEDED and self.baseClient.get_state() != actionlib_msgs.msg.GoalStatus.PREEMPTED:
             return False
 
         rospy.sleep(rospy.Duration.from_sec(0.3)) #avoid jumping out of a state immediately after entering it - actionlib bug
