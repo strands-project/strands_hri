393ce1f777e6e9e00b5845a8b92212b136817915
Marc Hanheide <marc@hanheide.net>
2014-05-16 12:03:37 +0100

    now remembers previous setting

--- strands_hri_utils/src/speak_webserver/webserver.py
+++ strands_hri_utils/src/speak_webserver/webserver.py
@@ -2,7 +2,7 @@
 
 from flask import Flask
 from flask import render_template
-from flask import request, redirect
+from flask import request, redirect, jsonify
 import roslib
 import rospy
 import actionlib
@@ -13,32 +13,65 @@ roslib.load_manifest('strands_hri_utils')
 app = Flask(__name__)
 
 
+param_prefix = '~speak_lines/'
+
+
+def get_param_key(n):
+    return param_prefix + get_id_str(n)
+
+
+def get_id_str(n):
+    return 'id'+str(n+1)
+
+
+def generate_lines(n):
+    lines = []
+    for i in range(n):
+        idv = get_id_str(i)
+        idp = get_param_key(i)
+        rospy.logdebug('read rosparam for ' + idp)
+        if rospy.has_param(idp):
+            sl = rospy.get_param(idp)
+        else:
+            sl = 'Test ' + str(i+1)
+        lines.append({'id': idv, 'value': sl})
+    return lines
+
+
+def save_line(id_value, text_value):
+    rospy.loginfo('store ros param for ' + id_value + ' as ' + text_value)
+    rospy.set_param(param_prefix + id_value, text_value)
+
+
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    l = generate_lines(5)
+    return render_template('index.html', lines=l)
 
 
 @app.route('/speak')
 def speak():
-    text = request.args.get('data')
+    text_value = request.args.get('text_value')
+    id_value = request.args.get('id_value')
     # the code below is executed if the request method
     # was GET or the credentials were invalid
-    rospy.loginfo('trying to say ' + text)
+    save_line(id_value, text_value)
+    rospy.loginfo('trying to say "' + text_value + '"')
     rospy.loginfo('waiting for say server')
     client = actionlib.SimpleActionClient(
         '/speak', ros_mary_tts.msg.maryttsAction)
 
     client.wait_for_server()
     g = ros_mary_tts.msg.maryttsGoal()
-    g.text = text
+    g.text = text_value
     rospy.loginfo('start speaking')
     client.send_goal(g)
     rospy.loginfo('started speaking')
     client.wait_for_result()
     rospy.loginfo('finished speaking')
-    return redirect('/')
+    return jsonify({'result': True})
 
 
 if __name__ == "__main__":
-	rospy.init_node('speak_webserver')
-	app.run(host='0.0.0.0', port=5000)
+    rospy.init_node('speak_webserver')
+    app.run(host='0.0.0.0', port=8080, use_reloader=False, debug=True)
