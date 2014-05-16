8952e7906ce10213d3d52052cbc964aa6c2027ac
Christian Dondrup <cdondrup@lincoln.ac.uk>
2014-05-16 14:33:21 +0100

    Made the button a parameter. Default: x

--- strands_hri_utils/scripts/gaze_switch.py
+++ strands_hri_utils/scripts/gaze_switch.py
@@ -6,6 +6,7 @@ import actionlib
 import strands_gazing.msg
 from scitos_apps_msgs.msg import action_buttons
 
+
 class GazeSwitch(object):
     # create messages that are used to publish feedback/result
 
@@ -14,20 +15,42 @@ class GazeSwitch(object):
         self.remote = True
 
         #Getting parameters
-        self.topic = rospy.get_param("~topic", "/upper_body_detector/closest_bounding_box_centre")
+        self.topic = rospy.get_param(
+            "~topic",
+            "/upper_body_detector/closest_bounding_box_centre"
+        )
+        self.button = rospy.get_param(
+            "~button",
+            "x"
+        )
 
         # Gaze client
         rospy.loginfo("%s: Creating gaze client", name)
-        self.gazeClient = actionlib.SimpleActionClient('gaze_at_pose', strands_gazing.msg.GazeAtPoseAction)
+        self.gazeClient = actionlib.SimpleActionClient(
+            'gaze_at_pose',
+            strands_gazing.msg.GazeAtPoseAction
+        )
         self.gazeClient.wait_for_server()
         self.goal = strands_gazing.msg.GazeAtPoseGoal()
         self.goal.topic_name = self.topic
         rospy.loginfo("%s: ...done", name)
 
-        rospy.Subscriber("/teleop_joystick/action_buttons", action_buttons, self.button_callback)
+        rospy.Subscriber(
+            "/teleop_joystick/action_buttons",
+            action_buttons,
+            self.button_callback
+        )
 
     def button_callback(self, action_buttons):
-        if(action_buttons.A):
+        rospy.loginfo("Button is %s", self.button.lower())
+        pressed = False
+        pressed = action_buttons.A if self.button.lower() == "a" else pressed
+        pressed = action_buttons.B if self.button.lower() == "b" else pressed
+        pressed = action_buttons.X if self.button.lower() == "x" else pressed
+        pressed = action_buttons.Y if self.button.lower() == "y" else pressed
+
+        if(pressed):
+            rospy.loginfo("Toggle gaze")
             if self.gazeClient.simple_state == actionlib.SimpleGoalState.ACTIVE:
                 self.gazeClient.cancel_all_goals()
             else:
