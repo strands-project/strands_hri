a9cf602ce5ccddc2ed30f2ecb353486bc9d76e3b
Alexander Hermans <hermans@vision.rwth-aachen.de>
2015-02-10 11:46:17 +0000

    Initial commit for the bellbot action server.

---
+++ src/bellbot_server.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python
+
+import roslib; roslib.load_manifest('bellbot_action_server')
+import rospy
+
+import threading
+
+import actionlib
+
+from bellbot_action_server.msg import *
+
+from bellbot_state_machine import BellbotStateMachine
+
+
+class BellbotServer(object):
+    # create messages that are used to publish feedback/result
+    _feedback = bellbot_action_server.msg.bellbotFeedback()
+    _result   = bellbot_action_server.msg.bellbotResult()
+    
+    def __init__(self, name):
+        self._action_name = name
+        self._as = actionlib.SimpleActionServer(self._action_name, object_search_action.msg.SearchAction, execute_cb=self.execute_cb, auto_start = False)
+
+        self._as.start()
+        rospy.loginfo('Started action server for the bellbot')
+        
+        
+    def execute_cb(self, goal):
+
+        rospy.loginfo('Received goal request: %s', goal)
+        
+        # helper variables
+        r = rospy.Rate(1)
+        self.success = True
+
+        # create a state machine
+        self.bellbot_sm = BellbotStateMachine()
+
+        # set userdata: pass the whole goal
+        self.bellbot_sm.set_sm_userdata(goal)        
+        # run the bellbot state machine with or without introspection server
+        # outcome = bellbot_sm.execute_sm()
+
+        smach_thread = threading.Thread(target = self.bellbot_sm.execute_sm_with_introspection)
+        smach_thread.start()
+
+        #outcome = self.bellbot_sm.execute_sm_with_introspection()
+        r.sleep()
+        
+        while self.bellbot_sm.get_sm().is_running() and not self.bellbot_sm.get_sm().preempt_requested():
+            # check that preempt has not been requested by the client
+            if self._as.is_preempt_requested():
+                rospy.loginfo('%s: Preempted' % self._action_name)
+                self._as.set_preempted()
+                self.bellbot_sm.get_sm().request_preempt()
+                self.success = False
+                break
+
+            r.sleep()
+
+        if self.success:
+            rospy.loginfo('%s: Succeeded' % self._action_name)
+
+
+if __name__ == '__main__':
+  rospy.init_node('bellbot_action')
+  BellbotServer(rospy.get_name())
+  rospy.spin()
---
+++ src/bellbot_state_machine.py
@@ -0,0 +1,170 @@
+#! /usr/bin/env python
+import rospy
+import smach
+import smach_ros
+import sys
+import getopt
+import random
+import json
+
+from agent import Agent
+
+import actionlib
+from actionlib_msgs.msg import *
+import topological_navigation.msg
+
+
+class BellbotStateMachine(Agent):
+
+    """
+    Definition of the bellbot state machine. 
+
+    """
+    def __init__(self):
+
+        # the super().__init__() methods calls make_sm(),
+        super(BellbotStateMachine,self).__init__()
+       
+    
+    def make_sm(self):
+        self.sm = smach.StateMachine(outcomes=['succeeded','aborted','preempted'])
+        
+        with self.sm:
+            
+            smach.StateMachine.add('Setup', Setup(), 
+                                   transitions={ 'succeeded': 'WaitingForGoal', 
+                                                 'aborted': 'aborted',
+                                                 'preempted': 'preempted' 
+                                               })
+            smach.StateMachine.add('WaitingForGoal', WaitingForGoal(), 
+                                   transitions={ 'succeeded': 'Guiding', 
+                                                 'aborted': 'aborted',
+                                                 'preempted': 'preempted' 
+                                               })
+            smach.StateMachine.add('Guiding', Guiding(), 
+                                   transitions={ 'succeeded': 'WaitingForGoal', 
+                                                 'aborted': 'aborted',
+                                                 'preempted': 'preempted' 
+                                               })
+            smach.StateMachine.add('WaitingForFeedback', WaitingForFeedback(), 
+                                   transitions={ 'succeeded': 'Setup', 
+                                                 'aborted': 'aborted',
+                                                 'preempted': 'preempted' 
+                                               })
+            return self.sm
+
+    def set_sm_userdata(self, goal):
+        """Set userdata for state machine."""
+        rospy.loginfo('Getting parameters from server')
+
+        self.sm.userdata.goal  = goal 
+
+        
+    def get_sm_userdata(self):
+        return self.sm.userdata
+
+#______________________________________________________________________________
+# behavior
+
+class Setup(smach.State):
+
+    """
+    Move the robot to a requested location to provide the Bellbot service.
+    """
+
+    def __init__(self):
+        smach.State.__init(self,
+                           outcomes=['succeeded', 'aborted', 'preempted'],
+                           input_keys=['goal'],
+                           output_keys=[])
+
+
+        rospy.on_shutdown(self._on_node_shutdown)
+        
+        self.client = actionlib.SimpleActionClient('topological_navigation', topological_navigation.msg.GotoNodeAction)
+        
+        self.client.wait_for_server()
+        
+        rospy.loginfo(" Setup ... Init done")
+        
+    def _on_node_shutdown(self):
+        self.client.cancel_all_goals()
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state %s', self.__class__.__name__)
+        navgoal = topological_navigation.msg.GotoNodeGoal()
+        
+        rospy.logging("Requesting Navigation to %s", userdata.goal.starting_waypoint_name)
+        navgoal.target = userdata.goal.starting_waypoint_name
+        
+        # Sends the goal to the action server.
+        self.client.send_goal(navgoal)
+
+        # Waits for the server to finish performing the action.
+        status=self.client.get_state()
+        while (status == GoalStatus.ACTIVE or status == GoalStatus.PENDING):
+            if self.preempt_requested(): 
+                self.service_preempt()
+                self.client.cancel_all_goals()
+                return 'preempted'
+            else:
+                status=self.client.get_state()
+                rospy.sleep(rospy.Duration.from_sec(0.01))
+                res=self.client.get_result()
+        
+        # Check if the naviation was successful 
+        navigation_success  = self.client.get_result()
+        if navigation_success:
+            return 'succeeded' 
+        else:
+            return 'aborted'
+
+class WaitingForGoal(smach.State):
+
+    """
+    Show the GUI and wait for input.
+    """
+
+    def __init__(self):
+        smach.State.__init(self,
+                           outcomes=['succeeded', 'aborted', 'preempted'],
+                           input_keys=[],
+                           output_keys=[])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state %s', self.__class__.__name__)
+        return 'succeeded' 
+
+class Guiding(smach.State):
+
+    """
+    Guide a person to the required destination.
+    """
+
+    def __init__(self):
+        smach.State.__init(self,
+                           outcomes=['succeeded', 'aborted', 'preempted'],
+                           input_keys=[],
+                           output_keys=[])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state %s', self.__class__.__name__)
+        return 'succeeded' 
+
+class WaitingForFeedback(smach.State):
+
+    """
+    Wait for feedback on this bellbot tour. 
+    """
+
+    def __init__(self):
+        smach.State.__init(self,
+                           outcomes=['succeeded', 'aborted', 'preempted'],
+                           input_keys=[],
+                           output_keys=[])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state %s', self.__class__.__name__)
+        return 'succeeded' 
+
+
