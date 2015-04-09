a92ffe95db9e3c265827175e3a6020f07cb6a1ef
Jaime Pulido Fentanes <pulidofentanes@gmail.com>
2015-04-09 15:52:10 +0100

    tiding up fixing files

--- bellbot_gui/setup.py
+++ bellbot_gui/setup.py
@@ -4,9 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    # #  don't do this unless you want a globally visible script
-    # scripts=['bin/myscript'],
-    packages=[],
+    packages=['bellbot_gui'],
     package_dir={'': 'src'}
 )
 
