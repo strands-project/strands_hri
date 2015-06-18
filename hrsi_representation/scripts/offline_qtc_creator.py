d7e5d2b376f07496c3d0742ba8d5da4dc8d20689
Christian Dondrup <cdondrup@lincoln.ac.uk>
2015-06-18 16:58:47 +0100

    Removed hmm creator and renamed train to offline_qtc_creator. Also made some changes to only convert to qtc and attempt to train an hmm.

--- hrsi_representation/scripts/offline_qtc_creator.py
+++ hrsi_representation/scripts/offline_qtc_creator.py
@@ -9,27 +9,32 @@ Created on Fri Jan 30 15:28:58 2015
 import rospy
 import argparse
 from hrsi_representation.file_input import FileInput
-from hrsi_representation.create_hmm import QTCHMM
 
 
-class Train(object):
+class OfflineQTCCreator(object):
     """trains hmm from raw data files"""
 
     def __init__(self, name, args):
-        """Creates a new instance of the train class
+        """Creates a new instance of the class
         """
         rospy.loginfo("Starting %s", name)
-        self.q = args.quantisation_factor
-        self.v = True  # args.validate # Has to be validated
-        self.n = args.no_collapse
-        self.d = args.distance_threshold
-        self.i = args.input
-        self.qsr = args.qsr
+        self.parameters = {
+            "quantisation_factor": rospy.get_param("~quantisation_factor", 0.01),
+            "validate":            rospy.get_param("~validate", True),
+            "no_collapse":         rospy.get_param("~no_collapse", False),
+            "distance_threshold":  rospy.get_param("~distance_threshold", 1.2)
+        }
+
+        self.input = rospy.get_param("~input")
+        self.output = rospy.get_param("~output")
+        self.qsr = rospy.get_param("~qsr", "qtcc")
+
+        if not self.input:
+            rospy.logfatal("No input path provided. Cannot load files.")
 
         self.file_input = FileInput()
-        self.qtc_hmm = QTCHMM()
 
-    def train(self):
+    def create(self):
         rospy.loginfo("Reading file '%s':" % self.i)
         qtc = self.file_input.convert(
             data=self.file_input.generate_data_from_input(path=self.i),
@@ -39,23 +44,21 @@ class Train(object):
             no_collapse=self.n,
             distance_threshold=self.d
         )
-        hmm = self.qtc_hmm.createHMM(qtc_seq=qtc, qtc_type=str(self.qsr))
-        return hmm, qtc
+        return qtc
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("qsr", help="choose qsr: qtcc|qtcc|qtcbc", type=str)
     parser.add_argument("-i", "--input", help="path under which to read csv files", type=str)
-    #parser.add_argument("--validate", help="validate state chain. Only QTC", action="store_true")
+    parser.add_argument("--output", help="path in which to store the resulting qtc files", type=str)
     parser.add_argument("--quantisation_factor", help="quantisation factor for 0-states.", type=float)
     parser.add_argument("--no_collapse", help="does not collapse similar adjacent states.", action="store_true")
     parser.add_argument("--distance_threshold", help="distance threshold for qtcb <-> qtcc transition. Only QTCBC", type=float)
     args = parser.parse_args()
 
-    rospy.init_node("train")
-    t = Train(rospy.get_name(), args)
-    hmm, qtc = t.train()
+    rospy.init_node("offline_qtc_creator")
+    t = OfflineQTCCreator(rospy.get_name(), args)
+    qtc = t.train()
     print qtc
-    print hmm
     #rospy.spin()
