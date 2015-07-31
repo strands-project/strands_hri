98e6372a8bf0c63ebff0e9d656753ac8c567dc3b
Christian Dondrup <cdondrup@lincoln.ac.uk>
2015-07-31 18:42:52 +0100

    Adding first version of qtcb based prediction of next action. Very simple but woks for pass-by scenarios and emulates the old human_aware behaviour of stopping.

---
+++ hrsi_prediction/scripts/state_predictor_simple.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Created on Mon Jul 27 16:48:09 2015
+
+@author: cdondrup
+"""
+
+import rospy
+from message_filters import Subscriber, TimeSynchronizer
+from hrsi_representation.msg import QTCArray
+from bayes_people_tracker.msg import PeopleTracker
+import json
+import numpy as np
+from random import uniform, choice
+from std_msgs.msg import String
+from hrsi_prediction.costmap_creator import CostmapCreator
+from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import PoseStamped
+
+
+class QTCStatePredictor(object):
+    def __init__(self, name):
+        rospy.loginfo("Starting %s..." % name)
+        self.cc = CostmapCreator(
+            rospy.Publisher("~map", OccupancyGrid, queue_size=10, latch=True),
+            rospy.Publisher("~origin", PoseStamped, queue_size=10),
+            100,
+            100
+        )
+        self.cnt = 0
+        self.qtc_states = self._create_qtc_states('b')
+        print self.qtc_states
+        self.transdict = self._create_transition_dict(self.qtc_states)
+        self.pub = rospy.Publisher("~prediction", String, queue_size=10)
+        subs = [
+            Subscriber(rospy.get_param("~qtc_topic", "/online_qtc_creator/qtc_array"), QTCArray),
+            Subscriber(rospy.get_param("~ppl_topic", "/people_tracker/positions"), PeopleTracker)
+        ]
+        ts = TimeSynchronizer(
+            fs=subs,
+            queue_size=30
+        )
+        ts.registerCallback(self.callback)
+        rospy.loginfo("... all done.")
+
+    def callback(self, qtc, ppl):
+        self.cnt += 1
+        data_buffer = {e.uuid: {"qtc": json.loads(e.qtc_serialised)[-1], "distance": ppl.distances[ppl.uuids.index(e.uuid)], "angle": ppl.angles[ppl.uuids.index(e.uuid)]} for e in qtc.qtc}
+#        rospy.loginfo("%s: TIME_MSG: %s, BUFFER: %s" % (str(self.cnt),str(qtc.header.stamp.to_sec()),data_buffer))
+#        for element in data_buffer.values():
+        element = data_buffer.values()[0] # Only taking the first detection into account for now
+        pred = [choice([-1,1]),element["qtc"][1]] if element["qtc"][0] == 0 else element["qtc"] # Always assume the robot moves
+        if element["distance"] <= 5.0:
+            if element["qtc"][0] == -1:
+                x = uniform(0,1)
+                prob = 2.0 / element["distance"]
+                prob = prob if prob <= 1.0 else 1.0
+                print "PROB: %s - %s = %s -> slow down: %s" %(x,prob,x-prob,x-prob<0.0)
+                if x - prob <= 0.0:
+                    pred = [0,element["qtc"][1]]
+                else:
+                    pred = [-1,element["qtc"][1]]
+            elif element["qtc"][0] == 0:
+                if element["qtc"][1] == 1:
+                    pred = [1,element["qtc"][1]]
+                elif element["qtc"][1] == 0 and element["distance"] > 2.0: # No one moves, take initiative
+                    pred = [-1,element["qtc"][1]]
+                else:
+                    pred = [0,element["qtc"][1]]
+
+        self.cc.publish(element["angle"], pred[0])
+
+        print "human-robot distance: %s, current qtc: %s, predicted qtc: %s" % (element["distance"], element["qtc"], pred)
+        self.pub.publish(self.qtc_states[0][self.qtc_states[1].index(pred)])
+
+
+    def _create_qtc_states(self, qtc_type):
+        ret_str = []
+        ret_int = []
+        try:
+            if qtc_type == "":
+                raise AttributeError()
+            elif qtc_type == 'b':
+                for i in xrange(1, 4):
+                    for j in xrange(1, 4):
+                        ret_int.append([i-2, j-2])
+                        ret_str.append(str(i-2) + "," + str(j-2))
+            elif qtc_type is 'c':
+                for i in xrange(1, 4):
+                    for j in xrange(1, 4):
+                        for k in xrange(1, 4):
+                            for l in xrange(1, 4):
+                                ret_int.append([i-2, j-2, k-2, l-2])
+                                ret_str.append(str(i-2) + "," + str(j-2) + "," + str(k-2) + "," + str(l-2))
+            elif qtc_type is 'bc':
+                for i in xrange(1, 4):
+                    for j in xrange(1, 4):
+                        ret_int.append([i-2, j-2, np.NaN, np.NaN])
+                        ret_str.append(str(i-2) + "," + str(j-2))
+                for i in xrange(1, 4):
+                    for j in xrange(1, 4):
+                        for k in xrange(1, 4):
+                            for l in xrange(1, 4):
+                                ret_int.append([i-2, j-2, k-2, l-2])
+                                ret_str.append(str(i-2) + "," + str(j-2) + "," + str(k-2) + "," + str(l-2))
+        except AttributeError:
+            rospy.logfatal("QTC type: %s not found" % qtc_type)
+            return None, None
+        return [s.replace('-1','-').replace('1','+') for s in ret_str], ret_int
+
+    def _create_transition_dict(self, qtc_tuple):
+        qtc = np.array(qtc_tuple[1])
+
+        trans = np.zeros((qtc.shape[0], qtc.shape[0]))
+        for i1 in xrange(qtc.shape[0]):
+            for i2 in xrange(i1+1, qtc.shape[0]):
+                trans[i1, i2] = np.nanmax(np.absolute(qtc[i1]-qtc[i2])) != 2
+                if trans[i1, i2] == 1:
+                    for j1 in xrange(qtc.shape[1]-1):
+                        for j2 in xrange(j1+1, qtc.shape[1]):
+                            if sum(np.absolute(qtc[i1, [j1, j2]])) == 1 \
+                                    and sum(np.absolute(qtc[i2, [j1, j2]])) == 1:
+                                if np.nanmax(np.absolute(qtc[i1, [j1, j2]]-qtc[i2, [j1, j2]])) > 0 \
+                                        and sum(qtc[i1, [j1, j2]]-qtc[i2, [j1,j2]]) != 1:
+                                    trans[i1, i2] = 5
+                                    break
+                        if trans[i1, i2] != 1:
+                            break
+                trans[i2, i1] = trans[i1, i2]
+
+        trans[trans != 1] = 0
+#        trans += np.dot(np.eye(qtc.shape[0]), 1)
+
+        return {k:np.array(qtc_tuple[0])[np.array(trans, dtype=bool)[i]] for i, k in enumerate(qtc_tuple[0])}
+
+
+if __name__ == "__main__":
+    rospy.init_node("qtc_state_predictor")
+    q = QTCStatePredictor(rospy.get_name())
+    rospy.spin()
---
+++ hrsi_prediction/src/hrsi_prediction/costmap_creator.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 30 16:07:34 2015
+
+@author: cdondrup
+"""
+
+import rospy
+from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import Pose, PoseStamped
+from tf.transformations import quaternion_from_euler
+import numpy as np
+import time
+
+
+class CostmapCreator(object):
+    def __init__(self, map_pub, origin_pub, width=100, height=100, max_costs=100, min_costs=0, resolution=0.05):
+        self._width = width
+        self._height = height
+        self._max_costs = max_costs
+        self._min_costs = min_costs
+        self._resolution = resolution
+        self._map_pub = map_pub #rospy.Publisher("~map", OccupancyGrid, queue_size=10, latch=True)
+        self._origin_pub = origin_pub #rospy.Publisher("~origin", PoseStamped, queue_size=10)
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, width):
+        self._width = width
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        self._height = height
+
+    @property
+    def max_costs(self):
+        return self._max_costs
+
+    @max_costs.setter
+    def max_costs(self, max_costs):
+        self._max_costs = max_costs
+
+    @property
+    def min_costs(self):
+        return self._min_costs
+
+    @min_costs.setter
+    def min_costs(self, min_costs):
+        self._min_costs = min_costs
+
+    @property
+    def resolution(self):
+        return self._resolution
+
+    @resolution.setter
+    def resolution(self, resolution):
+        self._resolution = resolution
+
+    def _fast_cost(self, angle, qtc_symbol):
+        start_loop = time.time()
+        cost_array = np.empty((self.width,self.height))
+        cost_array.fill(self.max_costs)
+        if qtc_symbol == 0:
+            return cost_array
+        if qtc_symbol == 1:
+            angle = angle + np.pi if angle < 0.0 else angle - np.pi
+
+        # The shit way of representing ans sending occupancy maps requires to create
+        # it like this and transpose it in the end.
+        cp = self._cartesian_product(
+            [
+                np.arange(-self.width/2,self.width/2, step=1),
+                np.arange(-self.height/2,self.height/2, step=1)
+            ]
+        )
+        polar = self._cartesian_to_polar(cp[:,0],cp[:,1])
+        idx = np.logical_and(
+            polar[0] <= min(self.width/2, self.height/2), #np.logical_or(polar[1] == np.pi/4, polar[1] == 0.0)
+            np.logical_and(
+                np.logical_or(
+                    polar[1] >= -np.pi/2 + angle,
+                    polar[1] <= -np.pi + (angle-np.pi/2) # Sign flip
+                ),
+                np.logical_or(
+                    polar[1] <= np.pi/2 + angle,
+                    polar[1] >= np.pi + (angle+np.pi/2) # Sign flip
+                )
+            )
+        ).reshape(self.width,self.height)
+        cost_array[idx] = self.min_costs
+        print "elapsed:", time.time() - start_loop
+        return cost_array.T # Transpose due to shit interpretation of the array
+
+
+    def _cartesian_product(self, arrays, out=None):
+        """
+        Generate a cartesian product of input arrays.
+
+        Parameters
+        ----------
+        arrays : list of array-like
+            1-D arrays to form the cartesian product of.
+        out : ndarray
+            Array to place the cartesian product in.
+
+        Returns
+        -------
+        out : ndarray
+            2-D array of shape (M, len(arrays)) containing cartesian products
+            formed of input arrays.
+
+        Examples
+        --------
+        >>> cartesian(([1, 2, 3], [4, 5], [6, 7]))
+        array([[1, 4, 6],
+               [1, 4, 7],
+               [1, 5, 6],
+               [1, 5, 7],
+               [2, 4, 6],
+               [2, 4, 7],
+               [2, 5, 6],
+               [2, 5, 7],
+               [3, 4, 6],
+               [3, 4, 7],
+               [3, 5, 6],
+               [3, 5, 7]])
+
+        """
+
+        arrays = [np.asarray(x) for x in arrays]
+        dtype = arrays[0].dtype
+
+        n = np.prod([x.size for x in arrays])
+        if out is None:
+            out = np.zeros([n, len(arrays)], dtype=dtype)
+
+        m = n / arrays[0].size
+        out[:,0] = np.repeat(arrays[0], m)
+        if arrays[1:]:
+            self._cartesian_product(arrays[1:], out=out[0:m,1:])
+            for j in xrange(1, arrays[0].size):
+                out[j*m:(j+1)*m,1:] = out[0:m,1:]
+        return out
+
+    def publish(self, angle, qtc_symbol):
+        o = OccupancyGrid()
+        o.header.stamp = rospy.Time.now()
+        o.header.frame_id = 'base_link'
+        o.info.resolution = self.resolution
+        o.info.height = self._height
+        o.info.width = self._width
+        o.info.origin = Pose()
+#        o.info.origin.position.x = 13.6
+#        o.info.origin.position.y = -1.3
+#        o.info.origin.orientation.w = 1
+
+#        rot = quaternion_from_euler(0.0, 0.0, -np.pi/2) # Rotate -90 degrees so 0.0 in polar is forward
+#        o.info.origin.orientation.z = rot[2]
+#        o.info.origin.orientation.w = rot[3]
+        o.info.origin.position.x -= (o.info.height/2) * o.info.resolution # Translate to be centered under robot
+        o.info.origin.position.y -= (o.info.width/2)  * o.info.resolution
+        p = PoseStamped(header=o.header, pose=o.info.origin)
+        self._origin_pub.publish(p)
+
+        o.data = self._fast_cost(angle, qtc_symbol).flatten(order='C')
+        self._map_pub.publish(o)
+
+    def _cartesian_to_polar(self, x, y):
+        rho = np.sqrt(np.power(x,2) + np.power(y,2))
+        phi = np.arctan2(y, x)
+        return(rho, phi)
+
+    def _polar_to_cartesian(self, rho, phi):
+        x = rho * np.cos(phi)
+        y = rho * np.sin(phi)
+        return(x.astype(int), y.astype(int))
